80541a9d32f7957e0d8ea4929f74169a6e82eb38
Mohammed Sohail <sohailsameja@gmail.com>
2017-01-27 23:02:53 +0300

    [Feature] Crude Sync

--- core/mongo.js
+++ core/mongo.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose');
 const rangi = require('rangi');
 const config = require('./config');
+const cron = require('node-cron');
+const Ban = require('../models/banmodel');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.MONGO_URI);
@@ -15,3 +17,19 @@ db.once('open', () => {
 });
 
 module.exports = db;
+
+cron.schedule('* 3 * * *', () => {
+    const sync = mongoose.createConnection('mongodb://sync:sync@ds029665.mlab.com:29665/banhammer');
+    const remoteList = sync.model('Ban');
+    remoteList.find({}, (err, docs) => {
+        Ban.collection.insert(docs, status)
+    })
+
+    function status(err, result) {
+        if (err && err.code == 11000) {
+            console.log(rangi.red(`Err`));
+        } else {
+            console.info(rangi.magenta(`Sync Successful!`));
+        }
+    }
+});
