d6b55cd71d5cb814ebb5f7f20bab4d73cfde6d12
Mohammed Sohail <sohailsameja@gmail.com>
2017-01-28 11:28:00 +0300

    [Feature] Inline Mode
    - Help is now fully inline
    - With toggle buttons

--- controllers/menu.js
+++ controllers/menu.js
@@ -44,6 +44,26 @@ const helpKeyboard = [[{
     callback_data: `help_it`
 }]]
 
+const inlineKeyboard = [[{
+    text: `🇬🇧 English`,
+    switch_inline_query_current_chat: `help en`
+}, {
+    text: `🇪🇸 Español`,
+    switch_inline_query_current_chat: `help es`
+}], [{
+    text: `🇧🇷 Português`,
+    switch_inline_query_current_chat: `help pt`
+}, {
+    text: `فارسی 🇮🇷`,
+    switch_inline_query_current_chat: `help fa`
+}], [{
+    text: `الرئيسية 🇸🇦`,
+    switch_inline_query_current_chat: `help ar`
+}, {
+    text: `🇮🇹 Italiano`,
+    switch_inline_query_current_chat: `help it`
+}]]
+
 const backKeyboard = [[{
     text: `🔙`,
     callback_data: `recycle_start`
@@ -448,4 +468,97 @@ bot.on('callback_query', msg => {
             }
         })
     }
+});
+
+bot.on('inline_query', function(msg) {
+    let langCode = msg.query.slice(5);
+    let lang = msg.query.slice(5);
+    if (lang == `en`) {
+        lang = locale.en
+    }
+    if (lang == `es`) {
+        lang = locale.es
+    }
+    if (lang == `pt`) {
+        lang = locale.pt
+    }
+    if (lang == `fa`) {
+        lang = locale.fa
+    }
+    if (lang == `en`) {
+        lang = locale.ar
+    }
+    if (lang == `es`) {
+        lang = locale.it
+    }    
+    let opts = [];
+    if (msg.query == `help ${langCode}`) {
+        opts = [
+            {
+                type: `article`,
+                id: `${langCode}_banhammer_help`,
+                title: lang.banhammer_help,
+                message_text: lang.banhammerinfo,
+                parse_mode: `Markdown`,
+                thumb_url: `http://emojipedia-us.s3.amazonaws.com/cache/30/73/3073d8609d9e23b21f89c53f83d2dea7.png`
+            },
+            {
+                type: `article`,
+                id: `${langCode}_superuser_help`,
+                title: lang.superuser_help,
+                message_text: lang.superuserinfo,
+                parse_mode: `Markdown`,
+                thumb_url: `http://emojipedia-us.s3.amazonaws.com/cache/07/82/078231fee00b5dfdf1307cdaf8306779.png`
+            },
+            {
+                type: `article`,
+                id: `${langCode}_globaladmin_help`,
+                title: lang.globaladmin_help,
+                message_text: lang.globaladmininfo,
+                parse_mode: `Markdown`,
+                thumb_url: `http://emojipedia-us.s3.amazonaws.com/cache/03/49/03492090c0e25801a2d526c09f1ea8af.png`
+            },
+            {
+                type: `article`,
+                id: `${langCode}_ingroup_help`,
+                title: lang.ingroup_help,
+                message_text: lang.ingroupinfo,
+                parse_mode: `Markdown`,
+                thumb_url: `http://emojipedia-us.s3.amazonaws.com/cache/f3/03/f3033f43d7254a7e483c6995c60b9bf9.png`
+            },
+            {
+                type: `article`,
+                id: `${langCode}_about_help`,
+                title: lang.about_help,
+                message_text: lang.aboutinfo,
+                parse_mode: `Markdown`,
+                thumb_url: `http://emojipedia-us.s3.amazonaws.com/cache/a3/88/a3881d9895bfa74d5f8c6acea0953c3d.png`
+                
+            },
+            {
+                type: `article`,
+                id: `${langCode}_credits_help`,
+                title: lang.credits_help,
+                message_text: lang.creditsinfo,
+                parse_mode: `Markdown`,
+                thumb_url: `http://emojipedia-us.s3.amazonaws.com/cache/51/5b/515b0cff9d179c6b7fa14d5ee54deebb.png`
+            }      
+
+        ]
+    } else {
+        opts = [
+            {
+                type: `article`,
+                id: `not_found`,
+                title: `Help: Not Found`,
+                message_text: `*Select Language:*`,
+                parse_mode: `Markdown`,
+                thumb_url: `http://emojipedia-us.s3.amazonaws.com/cache/fe/60/fe60e1bd45961cb128c4869269826d7f.png`,
+                reply_markup: {
+                    inline_keyboard: inlineKeyboard
+                }
+            }
+        ]
+    }
+    bot.answerInlineQuery(msg.id, opts);
 });
