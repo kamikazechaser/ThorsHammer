a6711bd67583d7b755c1dcd10e2d833d8f333c17
Mohammed Sohail <sohailsameja@gmail.com>
2017-01-29 00:03:39 +0300

    [Feature] Ngrok

--- core/telegram.js
+++ core/telegram.js
@@ -4,14 +4,36 @@ const Tg = require('node-telegram-bot-api');
 const config = require('./config');
 const moment = require('moment');
 const rangi = require('rangi');
+const ngrok = require('ngrok');
+
+let ngrokTunnel;
+
+const tunnelIt = () => {
+    ngrok.connect({
+        addr: 8443,
+        region: 'eu'
+    }, (err, url) => {
+        ngrokTunnel = url;
+        console.log(rangi.magenta(`Tunnel Connected At ${url}`))
+        bot.setWebHook(`${ngrokTunnel}/bot${config.BOT_TOKEN}`);
+    });
+}
+
+tunnelIt();
 
 const bot = new Tg(config.BOT_TOKEN, {
-    polling: true
+    webHook: {
+        host: ngrokTunnel,
+        port: 8443
+    },
+    onlyFirstMatch: true
 });
 
 bot.getMe().then(me => {
     console.log(rangi.cyan(`Bot Is Running => ${me.username}`));
-    bot.sendMessage(config.SUDO, `Bot Started!\n${moment().format('MMMM Do YYYY, h:mm:ss a')}`, {parse_mode: 'Markdown'});
+    bot.sendMessage(config.SUDO, `Bot Started!\n${moment().format('MMMM Do YYYY, h:mm:ss a')}`, {
+        parse_mode: 'Markdown'
+    });
 });
 
 module.exports = bot;
