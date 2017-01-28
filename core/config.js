2623757e9bc5be6bb8411b4c49693383b15e304a
Mohammed Sohail <sohailsameja@gmail.com>
2017-01-28 14:19:52 +0300

    [Fix] Removed Dev Tokens $ Consts

--- core/config.js
+++ core/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://test:test@ds021026.mlab.com:21026/hammertest',
-    BOT_TOKEN: process.env.BOT_TOKEN || '229775585:AAG-4LTVHyEw3jof7DLVbkoG90dEaT-juUY',
-    SUDO: process.env.SUDO || '135207785',
-    LOG_CHANNEL: process.env.LOG_CHANNEL || '-1001115245761'
+    MONGO_URI: process.env.MONGO_URI || '',
+    BOT_TOKEN: process.env.BOT_TOKEN || '',
+    SUDO: process.env.SUDO || '',
+    LOG_CHANNEL: process.env.LOG_CHANNEL || ''
 };
