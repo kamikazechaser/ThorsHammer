e3785a535783307949acfaa161fb708cc68b5be1
Mohammed Sohail <sohailsameja@gmail.com>
2017-02-10 17:50:25 +0300

    [Feature] Openshift

--- core/config.js
+++ core/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URI: process.env.MONGO_URI || '',
+    MONGO_URI: process.env.MONGODB_URL || '',
     BOT_TOKEN: process.env.BOT_TOKEN || '',
     SUDO: process.env.SUDO || '',
     LOG_CHANNEL: process.env.LOG_CHANNEL || ''
--- core/telegram.js
+++ core/telegram.js
@@ -1,13 +1,15 @@
 'use strict';
 
-const Tg = require('node-telegram-bot-api');
+const Tgfancy = require('tgfancy');
 const config = require('./config');
 const moment = require('moment');
 const rangi = require('rangi');
 
-const bot = new Tg(config.BOT_TOKEN, {
-    polling: true
-});
+const bot = new Tgfancy(config.BOT_TOKEN, {
+    tgfancy: {
+        openshiftWebHook: true,
+    },
+});;
 
 bot.getMe().then(me => {
     console.log(rangi.cyan(`Bot Is Running => ${me.username}`));
